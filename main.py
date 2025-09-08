a4276bf3d12defe48e5e8154beb482b4e1e802da
JulianDlc <ja.dlcastillo@gmail.com>
2025-09-08 10:12:22 -0600

    clase candidata

---
+++ main.py
@@ -0,0 +1,9 @@
+class Candidata:
+    def __init__(self, codigo, nombre, edad, institucion_educativa, municipio):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.edad = edad
+        self.institucion = institucion_educativa
+        self.municipio = municipio
+
+
